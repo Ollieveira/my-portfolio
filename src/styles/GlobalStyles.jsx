b5d6434c3cd296a1292a0e68ab0bf76a5596b110
Willys de Oliveira <willys_oliveira@hotmail.com>
2023-11-29 12:08:35 -0300

    feat: add cfg GlobalStyles component

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;  
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+  }
+`
+export default GlobalStyles
