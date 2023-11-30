b1332be6f6eb41845cbcdba629f7c5f85c2db69e
Willys de Oliveira <willys_oliveira@hotmail.com>
2023-11-30 11:14:30 -0300

    style: little adjust

--- src/components/Header/Header.styles.jsx
+++ src/components/Header/Header.styles.jsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components'
 
-const HeaderStyle = styled.header`
+const HeaderStyle = styled.header`  
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100px;
   background-color: #333;
-  color: #fff;  
+  color: #fff;
 `
 
 export default HeaderStyle
