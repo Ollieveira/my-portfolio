fbdf94c6717967a3fb815a54e45aaf1ee3677a28
Willys de Oliveira <willys_oliveira@hotmail.com>
2023-11-30 11:26:56 -0300

    style: remove icon wrapper

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,22 +5,7 @@ const SocialMediaWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 2.5rem;
-  padding: 2.5rem 0;
-`
-const IconContainer = styled.div`
-  border: 1px solid #fff;
-  border-radius: 100px;
-  width: 40px;
-  height: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: color 0.3s ease-in-out;
-
-  &:hover {
-    border: 1px solid #b3b3b3;
-    cursor: pointer;
-  }
+  padding: 1.5rem 0;
 `
 const SocialIcon = styled.a`
   color: #fff;
@@ -36,15 +21,9 @@ const SocialMedia = ({ socialIcons }) => {
   return (
     <SocialMediaWrapper>
       {socialIcons.map(item => (
-        <IconContainer>
-          <SocialIcon
-            href={item.link}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon style={{ fontSize: 20 }} icon={item.icon} />
-          </SocialIcon>
-        </IconContainer>
+        <SocialIcon href={item.link} target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon style={{ fontSize: 30 }} icon={item.icon} />
+        </SocialIcon>
       ))}
     </SocialMediaWrapper>
   )
